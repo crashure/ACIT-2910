69c7c18962714c35e1cb91628c925590f76c6f5c
Cheuk Hin (Kelvin) Lau <clau128@my.bcit.ca>
2017-05-25 16:08:41 -0700

    Allows for open and close (#152)
    * final touches to profile and admin pages
    
    * hopefully allows for open and close
    
    * fixed syntax error

--- index.js
+++ index.js
@@ -83,10 +83,11 @@ app.get("/loginPage", function(req,resp){
 app.get("/menu", function(req, resp){
     if(req.session.ids){
         /* DONT DELETE THESE COMMENTS */
-//        if (storeStatus == false) {
-//            resp.sendFile(pF+"/closed.html");
-//        } else {
+        if (storeStatus == false) {
+            resp.sendFile(pF+"/closed.html");
+        } else {
             resp.sendFile(pF+"/menu.html");
+        }
     } else {
         resp.sendFile(pF+"/login.html")
     }
